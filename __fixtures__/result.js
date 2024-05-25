dddf694c6f865039fff3bf31abcbff2bd739c4a2
Your Name~rm <you@example.com>
2024-05-25 13:31:32 +0300

    step5 i have fixed all mistakes

--- __fixtures__/result.js
+++ __fixtures__/result.js
@@ -1,10 +1,10 @@
 const result = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 50
- + verbose: true
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 50
+  + verbose: true
 }`;
 
 export default result;
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,17 @@
 import gendiff from '../src/index.js';
 import result from '../__fixtures__/result.js';
+// import path from 'node:path';
 
 test('check json plain', () => {
-  expect(gendiff('file1.json', 'file2.json')).toEqual(result);
+//   expect(gendiff('file1.json', 'file2.json')).toEqual(result);
+  expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
 });
+
+// const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
+
+// test('genDiff json test', () => {
+//   const path1 = getFixturePath('file1.json');
+//   const path2 = getFixturePath('file2.json');
+//   const gendiffResult = genDiff(path1, path2);
+//   expect(gendiffResult).toEqual(expectedResult);
+// });
