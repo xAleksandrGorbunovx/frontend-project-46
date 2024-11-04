5841a301130b07bb7847bd87adcec2a08ef20389
Your Name~rm <you@example.com>
2024-11-04 17:18:19 +0300

    fix some mistakes

--- src/buildAST.js
+++ src/buildAST.js
@@ -6,8 +6,8 @@ const buildAST = (data1, data2) => {
 
   const sortedKeys = _.sortBy(_.union(data1Keys, data2Keys));
 
-  // переделал на объект  в котором хранятся 
-  //различные методы, обрабатывающие возможные варианты изменений:
+  // переделал на объект  в котором хранятся
+  // различные методы, обрабатывающие возможные варианты изменений:
 
   const children = sortedKeys.map((key) => {
     const handlers = {
@@ -15,18 +15,20 @@ const buildAST = (data1, data2) => {
       removed: () => ({ type: 'removed', key, value: data1[key] }),
       nested: () => ({ type: 'nested', key, children: buildAST(data1[key], data2[key]) }),
       unchanged: () => ({ type: 'unchanged', key, value: data1[key] }),
-      changed: () => ({ type: 'changed', key, oldValue: data1[key], newValue: data2[key] }),
+      changed: () => ({
+        type: 'changed', key, oldValue: data1[key], newValue: data2[key],
+      }),
     };
-  
+
     if (!_.has(data1, key)) return handlers.added();
     if (!_.has(data2, key)) return handlers.removed();
-    
+
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) return handlers.nested();
     if (_.isEqual(data1[key], data2[key])) return handlers.unchanged();
-    
+
     return handlers.changed();
   });
-  
+
   return children;
 };
 
