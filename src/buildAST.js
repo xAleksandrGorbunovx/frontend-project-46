67fbb1d12d6147df8b1a5461342a5db5ac0bd7ad
Your Name~rm <you@example.com>
2024-11-04 17:12:57 +0300

    remade buildASTt for object programming

--- src/buildAST.js
+++ src/buildAST.js
@@ -5,47 +5,28 @@ const buildAST = (data1, data2) => {
   const data2Keys = _.keys(data2);
 
   const sortedKeys = _.sortBy(_.union(data1Keys, data2Keys));
-  // const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-  // console.log('data1Keys----', data1Keys);
-  // console.log('data2Keys----', data2Keys);
-  // console.log('sortedKeys----', sortedKeys);
+
+  // переделал на объект  в котором хранятся 
+  //различные методы, обрабатывающие возможные варианты изменений:
 
   const children = sortedKeys.map((key) => {
-    if (!_.has(data1, key)) {
-      return {
-        type: 'added',
-        key,
-        value: data2[key],
-      };
-    }
-    if (!_.has(data2, key)) {
-      return {
-        type: 'removed',
-        key,
-        value: data1[key],
-      };
-    }
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return {
-        type: 'nested',
-        key,
-        children: buildAST(data1[key], data2[key]),
-      };
-    }
-    if (_.isEqual(data1[key], data2[key])) {
-      return {
-        type: 'unchanged',
-        key,
-        value: data1[key],
-      };
-    }
-    return {
-      type: 'changed',
-      key,
-      oldValue: data1[key],
-      newValue: data2[key],
+    const handlers = {
+      added: () => ({ type: 'added', key, value: data2[key] }),
+      removed: () => ({ type: 'removed', key, value: data1[key] }),
+      nested: () => ({ type: 'nested', key, children: buildAST(data1[key], data2[key]) }),
+      unchanged: () => ({ type: 'unchanged', key, value: data1[key] }),
+      changed: () => ({ type: 'changed', key, oldValue: data1[key], newValue: data2[key] }),
     };
+  
+    if (!_.has(data1, key)) return handlers.added();
+    if (!_.has(data2, key)) return handlers.removed();
+    
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) return handlers.nested();
+    if (_.isEqual(data1[key], data2[key])) return handlers.unchanged();
+    
+    return handlers.changed();
   });
+  
   return children;
 };
 
